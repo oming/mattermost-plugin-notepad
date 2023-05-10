d7feeafefa0f34520c497916115f292fa3eb8767
안효상 <hsan@wesome.co.kr>
2023-05-10 12:35:37 +0900

    lint 오류 수정

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,53 +1,53 @@
-import React from "react";
-import { Store, Action } from "redux";
+import React from 'react';
+import {Store, Action} from 'redux';
 
-import { GlobalState } from "mattermost-redux/types/store";
+import {GlobalState} from 'mattermost-redux/types/store';
 
-import manifest from "./manifest";
-import Client from "./client";
+import manifest from './manifest';
+import Client from './client';
 
 // eslint-disable-next-line import/no-unresolved
-import { PluginRegistry } from "./types/mattermost-webapp";
-import { getServerRoute } from "./selectors";
+import {PluginRegistry} from './types/mattermost-webapp';
+import {getServerRoute} from './selectors';
 
-import reducer from "./reducer";
+import reducer from './reducer';
 
-import BookmarkIcon from "./components/icons/icons";
-import RHSView from "./components/right_hand_sidebar/components";
+import BookmarkIcon from './components/icons/icons';
+import RHSView from './components/right_hand_sidebar/components';
 
 export default class Plugin {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
-  public async initialize(
-    registry: PluginRegistry,
-    store: Store<GlobalState, Action<Record<string, unknown>>>
-  ) {
-    Client.setServerRoute(getServerRoute(store.getState()));
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+    public async initialize(
+        registry: PluginRegistry,
+        store: Store<GlobalState, Action<Record<string, unknown>>>,
+    ) {
+        Client.setServerRoute(getServerRoute(store.getState()));
 
-    // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
-    // registry.registerReducer(reducer);
+        // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+        // registry.registerReducer(reducer);
 
-    const { toggleRHSPlugin } = registry.registerRightHandSidebarComponent(
-      RHSView,
-      "Bookmark"
-    );
+        const {toggleRHSPlugin} = registry.registerRightHandSidebarComponent(
+            RHSView,
+            'Bookmark',
+        );
 
-    registry.registerChannelHeaderButtonAction(
-      <BookmarkIcon />,
-      () => store.dispatch(toggleRHSPlugin),
-      "Bookmark",
-      "View Bookmark"
-    );
-  }
+        registry.registerChannelHeaderButtonAction(
+            <BookmarkIcon/>,
+            () => store.dispatch(toggleRHSPlugin),
+            'Bookmark',
+            'View Bookmark',
+        );
+    }
 
-  uninitialize() {
+    uninitialize() {
     // No clean up required.
-  }
+    }
 }
 
 declare global {
-  interface Window {
-    registerPlugin(id: string, plugin: Plugin): void;
-  }
+    interface Window {
+        registerPlugin(id: string, plugin: Plugin): void;
+    }
 }
 
 window.registerPlugin(manifest.id, new Plugin());
